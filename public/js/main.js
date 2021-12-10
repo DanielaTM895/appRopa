78cca4cdab3dcec651d1a43ec98e4346f9500980
Daniela Tellez <daniela.tellez@utxicotepec.edu.mx>
2021-12-10 15:12:37 -0600

    ahora si

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,4 @@
-const promiseOfSomeData = fetch(
-  "https://practica-servidor.herokuapp.com/api/ropa"
-)
+const promiseOfSomeData = fetch("https://ropaserver.herokuapp.com/api/ropa")
   .then((r) => r.json())
   .then((data) => {
     console.log("in async");
