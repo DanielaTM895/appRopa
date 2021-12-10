41e2d056c9b3faabc144f4c2ab71e1541bdfd617
Daniela Tellez <daniela.tellez@utxicotepec.edu.mx>
2021-12-10 14:42:42 -0600

    si

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -1,6 +1,11 @@
 const d = document;
 
-const data = fetch("https://ropaserver.herokuapp.com/api/ropa")
+const data = fetch("https://ropaserver.herokuapp.com/api/ropa", {
+  mode: "cors",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+})
   .then((r) => r.json())
   .then((data) => {
     console.log("in async");
