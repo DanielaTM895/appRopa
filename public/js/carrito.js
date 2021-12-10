fcf0f6f57d51633786c5b1ed520bb04d920ce042
Daniela Tellez <daniela.tellez@utxicotepec.edu.mx>
2021-12-10 14:16:00 -0600

    Updates

---
+++ public/js/carrito.js
@@ -0,0 +1,83 @@
+const d = document;
+
+const data = fetch("https://practica-servidor.herokuapp.com/api/ropa")
+  .then((r) => r.json())
+  .then((data) => {
+    console.log("in async");
+    return data;
+  });
+
+const formB = d.querySelector(".formBusqueda");
+const formC = d.querySelector(".formCarrito");
+const formP = d.querySelector(".formPedido");
+
+formC.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let id = d.getElementById("id").value;
+  console.log(id);
+});
+
+formP.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let id = d.getElementById("id").value;
+  console.log(id);
+});
+
+formB.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let id = d.getElementById("id").value;
+  console.log(id);
+});
+
+let pintar = "";
+
+const buscar = d.getElementById("inputBuscar");
+buscar.addEventListener("keyup", async () => {
+  let grid = d.querySelector(".inicio");
+
+  let txt = d.getElementById("inputBuscar").value;
+  let sData = await data;
+
+  let texto = capita(txt);
+
+  var hasClase = grid.classList.contains("ropaGrid");
+
+  if (!hasClase) {
+    grid.className += " ropaGrid";
+  }
+
+  sData.map((data) => {
+    if (texto !== "" && data.nombre.includes(texto)) {
+      let card = `<div class="ropaCard">
+            <div class=" item card" style="width: 14rem">
+              <img src="${data.imagen}"  class=" item-image card-img-top ropaImage" alt="Imagen de ropa" />
+              <div class="card-body">
+                <h5 class="card-title item-title">${data.nombre}</h5>
+                <p class="item-price card-text">
+                  ${data.descripcion}
+                </p>
+                <p class="card-text">
+                  $${data.precio}
+                </p>
+                <div class="card-body">
+                  <button onclick="obtener(${data.id})" class="btn btn-outline-primary btn-sm" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom">Ver articulo</button>
+                </div>
+              </div>
+            </div>
+          </div>`;
+
+      pintar = pintar + card;
+
+      console.log(pintar);
+
+      grid.innerHTML = pintar;
+    }
+  });
+});
+
+function buscarRopa(texto) {
+  let ropaB = "";
+}
+function capita(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
---
+++ public/js/main.js
@@ -0,0 +1,99 @@
+const promiseOfSomeData = fetch(
+  "https://practica-servidor.herokuapp.com/api/ropa"
+)
+  .then((r) => r.json())
+  .then((data) => {
+    console.log("in async");
+    return data;
+  });
+
+let espacioDama = document.getElementById("espacioDama");
+let damaTemplate = "";
+let espacioCaballero = document.getElementById("espacioCaballero");
+let caballeroTemplate = "";
+let espacioInfantil = document.getElementById("espacioInfantil");
+let infantilTemplate = "";
+
+window.onload = async () => {
+  let someData = await promiseOfSomeData;
+  console.log("onload: " + someData);
+  someData.map((p) => {
+    console.log(p.categoria);
+    let card = `<div class="ropaCard">
+            <div class=" item card" style="width: 14rem">
+              <img src="${p.imagen}"  class=" item-image card-img-top ropaImage" alt="Imagen de ropa" />
+              <div class="card-body">
+                <h5 class="card-title item-title">${p.nombre}</h5>
+                <p class="item-price card-text">
+                  ${p.descripcion}
+                </p>
+                <p class="card-text">
+                  $${p.precio}
+                </p>
+                <div class="card-body">
+                  <button onclick="obtener(${p.id})" class="btn btn-outline-primary btn-sm" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom">Ver articulo</button>
+                </div>
+              </div>
+            </div>
+          </div>`;
+
+    switch (p.categoria) {
+      case "dama":
+        damaTemplate = damaTemplate + card;
+        break;
+      case "caballero":
+        caballeroTemplate = caballeroTemplate + card;
+        break;
+      case "infantil":
+        infantilTemplate = infantilTemplate + card;
+        break;
+      default:
+        break;
+    }
+
+    espacioDama.innerHTML = damaTemplate;
+    espacioCaballero.innerHTML = caballeroTemplate;
+    espacioInfantil.innerHTML = infantilTemplate;
+  });
+};
+
+let newServiceWorker;
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then((registerEvent) => {
+        registerEvent.addEventListener("updatefound", () => {
+          newServiceWorker = registerEvent.installing;
+
+          newServiceWorker.addEventListener("statechange", () => {
+            /* if (newServiveWorker.state === 'installed') {
+
+            } */
+            switch (newServiceWorker.state) {
+              case "installed":
+                showSnackbarUpdate();
+                break;
+            }
+          });
+        });
+      });
+  });
+}
+
+function showSnackbarUpdate() {
+  // Get the snackbar DIV
+  let x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+}
+
+let launchUpdate = document.getElementById("launchUpdate");
+launchUpdate.addEventListener("click", () => {
+  newServiceWorker.postMessage({
+    action: "skipWaiting",
+  });
+  window.reload();
+});
